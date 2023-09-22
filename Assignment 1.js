3b01dbfdc5a8b19d5af65f4e304411ae2b2abb74
idolg1982 <60248847+idolg1982@users.noreply.github.com>
2023-09-22 11:55:25 -0700

    Initial Commit

---
+++ Assignment 1.js
@@ -0,0 +1,119 @@
+//1 Convert the following highlighted identifiers to Camel Case notation:
+
+/*let someMonth;
+function theMonth() {}
+let currentMonth;
+let summerMonth;
+let myLibraryFunction; */
+
+//2 Examples of literal expressions:
+/*let numericLiteral = 42; 
+let stringLiteral = "Hello, World!"; 
+let booleanLiteral = true; 
+let nullLiteral = null; */
+
+/*3 Examples of complex/variable expressions:
+let complexExpression1 = (5 + 3) * 2; 
+let complexExpression2 = "Hello" + " " + "Zak"; */
+
+/*4 Declare (but do not assign) 9 variables using Camel Casing and Hungarian Notation:
+let strFirstName, strLastName, strAddress, strCity, strState, strZipCode, intYourAge, strReferralSource, boolMayWeContactYou;*/
+
+/*5 Assign values to three of the variables in three different ways:
+strFirstName = "John"; 
+strLastName = "Doe";
+strAddress = "123 Main St";
+
+/*6 Create a variable, add a number and a string, and display the coerced result in the console:
+let result = 10 + " apples";
+console.log(result); */
+
+/*7 Create two variables, add a Boolean and a string to the first, add a number and a Boolean to the second, and display the coerced results:
+let var1 = true + " is a boolean"; 
+let var2 = 42 + false; 
+console.log(var1);
+console.log(var2);*/
+
+/*8 Is the following string literal valid? If not, how would you fix it?
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'
+console.log(someString)
+
+No, its not valid.  It needs to have escape charectors to work or to use  ` ` instead of single qouates */
+
+/*9 Create a variable that produces a null value in the console window:
+let nullVariable = null;
+console.log(nullVariable); 
+
+// Create a variable that produces an undefined value in the console window:
+let undefinedVariable;
+console.log(undefinedVariable); 
+
+/*10 Use typeof operator on various literals:
+console.log(typeof "string"); // Output: "string"
+console.log(typeof 42); // Output: "number"
+console.log(typeof true); // Output: "boolean"
+console.log(typeof null); // Output: "object"
+console.log(typeof undefined); // Output: "undefined" */
+
+/*11 Use concatenation to display text in an alert box:
+let name = "Ivan Dolgonos";
+let course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+
+/*12 13 Declare a variable called name and set it equal to your name.
+Substitute your name in the previous alert string with the variable instead.
+Declare a variable called course and set it equal to “JavaScript”.
+Rework your alert string so that it displays the string of text but using the variables as opposed to hard coded text.:
+let name = "Ivan Dolgonos";
+let course = "JavaScript");
+alert("Hello " + name + ". Welcome to the " + course + " class!"); */
+
+/*14 Rework the above string so that a line break is added right before the word “Welcome”. Your alert box should display as follows:
+alert("Hello " + name + ". \nWelcome to the " + course + " class!"); */
+
+/*15 16 Replace hardcoded name and course with prompts:
+name = prompt("Please enter your name:");
+course = prompt("Please enter the class you are taking:");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+/* 17 18 19 20 Variable declarations and calculations:
+let x = 10;
+let y = 20;
+console.log(x + y); // Output: 30 
+
+x = 20;
+x += 20;
+console.log(x); // Output: 40
+
+x = 20;
+x *= 5;
+console.log(x); // Output: 100
+
+x = 20 % 3;
+x /= 1;
+console.log(x); // Output: 2*/
+
+/*21 Example of true application with comparison and logical operators:
+let a = 5;
+let b = 3;
+let c = 7;
+
+if (a > b && c > a) {
+    console.log("True");
+} else {
+    console.log("False");
+}*/
+
+/*22 Example of false application with different operators:
+let p = 10;
+let q = 20;
+let r = 5;
+
+if (p === q || r < p) {
+    console.log("True");
+} else {
+    console.log("False");
+}*/
+
+
